525c523bf561c5fb98d17ce62d4800f1c3e0af5d
Abdallahtrd <abdallahnura447@gmail.com>
2026-03-25 17:46:33 +0100

    refactor: improve CLI output and organixe analysis into main function

--- number-analyzer-cli.py
+++ number-analyzer-cli.py
@@ -4,14 +4,12 @@ def calculate_average(numbers): #This function calculates the average.
     total_sum = sum(numbers)
     num_count = len(numbers)
     average = total_sum / num_count
-    print("Your average is ", average)
     return average
 
 def calculate_sum(numbers):#This function calculates the sum of numbers.
     total_sum = 0
     for number in numbers:
         total_sum += number
-    print("Your sum is", total_sum)
     return total_sum 
     
 def calculate_range(numbers):#This function calculates the difference (range).
@@ -23,57 +21,50 @@ def calculate_range(numbers):#This function calculates the difference (range).
         if number < min_num:
             min_num = number
     difference = max_num - min_num
-    print("Your range is", difference)
     return difference
 
 def number_count(numbers):#This function checks the amount of digits in the list.
     count = 0
     for number in numbers:
         count += 1
-    print("Total numbers entered", count)
     return count
 
 def above_average(numbers, average):#This function calculates and prints all numbers above average.
-    abv_avglist = []#This stores all the numbers above average.
+    abv_avg_list = []#This stores all the numbers above average.
     for number in numbers:
         if number > average:
-            abv_avglist.append(number)
-    return abv_avglist
+            abv_avg_list.append(number)
+    return abv_avg_list
 
 def below_average(numbers, average):#This function calculates and prints all numbers below average.
-    below_average = []
+    below_average_list = []
     for number in numbers:
         if number < average:
-            below_average.append(number)
-    return below_average
+            below_average_list.append(number)
+    return below_average_list
 
 def equal_average(numbers, average):#This function checks for any number equal to average.
-    equal_average = []
+    equal_average_list = []
     for number in numbers:
         if number == average:
-            equal_average.append(number)
-    return equal_average
-        
+            equal_average_list.append(number)
+    return equal_average_list
 
-
-while True: #This loop makes he program run
-    choice = input("Use Add number(a) or stop(s) or analyze(z) or clear list(c)").lower()
-    if choice == 'a':#This is the option that adds numbers to the list
-        try:
-            number = int(input("Please add a number. "))
-            numbers.append(number)
-        except ValueError:
-            print("This is not a number.")
-    elif choice == 'z':#This is the option thay analyzes numbers in the list.
-        if not numbers:
+def main():
+    
+    if not numbers:
             print("Your list is Empty!!")
-        else:
+    else:
             print("Your list is:",numbers)
             avg = calculate_average(numbers)
-            calculate_sum(numbers)
-            calculate_range(numbers)
-            number_count(numbers)
+            calc_sum = calculate_sum(numbers)
+            print("The sum is:", calc_sum)
+            calc_range = calculate_range(numbers)
+            print("The range is:", calc_range)
+            num_count = number_count(numbers)
+            print("The amount of digits entered are:", num_count)
             eql_avg = equal_average(numbers, avg)
+    
             if not eql_avg:
                 print("There are no numbers equal to average")
             else:
@@ -93,7 +84,19 @@ while True: #This loop makes he program run
             else:
                 print("These numbers are below average:", below)
 
-        
+
+
+while True: #This loop makes he program run
+    choice = input("Use Add number(a) or stop(s) or analyze(z) or clear list(c)").lower()
+    if choice == 'a':#This is the option that adds numbers to the list
+        try:
+            number = int(input("Please add a number. "))
+            numbers.append(number)
+        except ValueError:
+            print("This is not a number.")
+    elif choice == 'z':#This is the option thay analyzes numbers in the list.
+        main()
+    
     elif choice == 'c':#This is the option that clears the list
         numbers.clear()
         print("Your history has been cleared!!")
